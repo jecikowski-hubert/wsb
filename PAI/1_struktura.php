e6b2038dacd6b2e01afea0267f832eedd3375194
jecikowski-hubert <h.jecikowski@gmail.com>
2020-03-07 11:06:46 +0100

    first file

---
+++ PAI/1_struktura.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+<head>
+<meta charset="UTF-8">
+<title> Struktura dokumentu </title>
+</head>
+
+<body>
+<?php
+echo"test";
+
+?>
+
+
+
+</body>
+</html>
