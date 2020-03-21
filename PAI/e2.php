6dd2835fac850cce557db86aca56d075a30378da
jecikowski-hubert <h.jecikowski@gmail.com>
2020-03-21 13:24:29 +0100

    1

--- PAI/e2.php
+++ PAI/e2.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <table>
+<tr>
+    <td>1.1</td>
+</tr>
+<tr>
+    <td>1.2</td>
+</tr>
+<tr>
+    <td>2.2</td>
+</tr>
+<tr>
+    <td>3.1</td>
+</tr>
+<tr>
+    <td>4.1</td>
+</tr>
 
 </table>
 
